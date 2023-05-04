3b5a7edeb69c61f23206a664e06ddb06c2b6766f
Gyu Jeong Kim <gjkim@beyless.com>
2023-05-04 11:27:34 +0900

    useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CounterContainer from './containers/CounterContainer';
+import { useQuery } from 'react-query'
+import { getProducts } from './api/api'
 
-function App() {
+interface Product {
+	title: string;
+	id: number;
+	category: string;
+	description: string;
+	image: string;
+	price: number;
+	rating: object;
+}
+
+const App = () => {
+	const { data, isLoading, isError } = useQuery<Array<Product>>('products', getProducts, {
+		staleTime: 30000,
+		cacheTime: 30000
+	});
+
+	if (isLoading) {
+    return <h4>Loading</h4>
+  }
+  if (isError) {
+    return <h4>Something went wrong !!</h4>
+  }
+	
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 				<CounterContainer />
       </header>
+			{data?.map((product: Product) => (
+        <div key={product.id}>
+          <h5>{product.title}</h5>
+        </div>
+      ))}
     </div>
   );
 }
